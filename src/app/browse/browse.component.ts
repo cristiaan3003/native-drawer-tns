3b9c31e06ad06ff817a0496174ba6391d4bc277b
cristiaan3003 <cristiaan3003@gmail.com>
2019-12-29 18:41:37 -0300

    integrando google maps plugin

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
+import {registerElement} from "nativescript-angular/element-registry"
 import * as app from "tns-core-modules/application";
+
+registerElement("MapView", () => require("nativescript-google-maps-sdk").MapView);
+var gmaps = require("nativescript-google-maps-sdk");
+
 
 @Component({
     selector: "Browse",
     templateUrl: "./browse.component.html"
 })
 export class BrowseComponent implements OnInit {
+    @ViewChild("MapView", null) mapView: ElementRef;
 
     constructor() {
         // Use the component constructor to inject providers.
@@ -19,5 +25,16 @@ export class BrowseComponent implements OnInit {
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
+    }
+
+    onMapReady(args) {
+        console.log("Map Ready!!");
+        var mapView= args.object;
+        var marker = new gmaps.Marker();
+        marker.position = gmaps.Position.positionFromLatLng(10.598081,-71.633228);
+        marker.title= "Colombia";
+        marker.snippet = "Bogota";
+        marker.userData = { index:1};
+        mapView.addMarker(marker);
     }
 }
