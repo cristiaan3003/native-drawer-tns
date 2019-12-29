f75a7aa39936f3a1b11e06dae7b4fcdd4a0b0a63
cristiaan3003 <cristiaan3003@gmail.com>
2019-12-29 17:29:58 -0300

    camera

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
+import * as camera from "nativescript-camera";
+import { Image } from "tns-core-modules/ui/image";
+import { ImageAsset } from "tns-core-modules/image-asset/image-asset";
 
 @Component({
     selector: "Home",
@@ -20,4 +23,24 @@ export class HomeComponent implements OnInit {
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
     }
+
+    onButtonTap() {
+        camera.requestCameraPermissions().then(
+            function success() {
+                const options = { width: 300, height: 300, keepAspectRatio: false, saveToGallery: true};
+                camera.takePicture(options)
+                      .then((imageAsset) => {
+                          console.log("Tamaño: " + imageAsset.options.width + "x" + imageAsset.options.height);
+                          console.log("keepAspectRatio: " + imageAsset.options.keepAspectRatio);
+                          console.log("Foto guardada");
+                      }).catch((err) => {
+                          console.log("Error -> " + err.message);
+                      });
+            },
+            function failure () {
+                console.log("Permiso de camara no aceptado por el usuario");
+            }
+        );
+    }
+
 }
