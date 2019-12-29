867df20158f37c91bfd0893ab15e6504b89a1b11
cristiaan3003 <cristiaan3003@gmail.com>
2019-12-29 10:25:57 -0300

    local sql lite and express ngrox

--- src/app/domain/noticias.service.ts
+++ src/app/domain/noticias.service.ts
@@ -1,11 +1,41 @@
 import {Injectable} from "@angular/core";
 import {getJSON,request} from "tns-core-modules/http";
+const sqlite = require("nativescript-sqlite");
 
 
 Injectable()
 export class NoticiasService{
     //private noticias: Array<string> = [];
-    api: string = "https://8de11800.ngrok.io";
+    api: string = "https://a6eaddd5.ngrok.io";
+
+    constructor(){
+        this.getDb((db) => {
+            console.dir(db);
+            db.each("select * from logs",
+            (err, fila) => console.log("fila" , fila),
+            (err, totales) => console.log("filas totales: ", totales));
+        }, () => console.log("error on getDB"));
+    }
+
+
+    getDb(fnOk,fnError){
+        return new sqlite("mi_db_logs",(err,db) => {
+            if(err){
+                console.log("error al abrir db!", err);
+            }else {
+                console.log("esta la db abierta:", db.isOpen() ? "Si": "No");
+                db.execSQL("CREATE TABLE IF NOT EXISTS logs (id INTEGER PRIMARY KEY AUTOINCREMENT,texto TEXT)")
+                .then((id) => {
+                    console.log("CREATE TABLE OK");
+                    fnOk(db);
+                },(error) => {
+                    console.log("CREATE TABLE ERROR", error);
+                    fnError(error);
+                });
+            }
+        });
+    }
+
 
 
     agregar(s: string){
@@ -24,6 +54,11 @@ export class NoticiasService{
     }
     
     buscar(s: string){
+        this.getDb((db) => {
+            db.execSQL("insert into logs (texto) values (?)", [s],
+            (err,id) => console.log("nuevo id" , id));
+        }, () => console.log("error on getDB"));
+        
         return getJSON(this.api + "/get?q="+s);
     }
 }
